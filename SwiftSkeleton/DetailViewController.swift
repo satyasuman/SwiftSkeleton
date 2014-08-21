824dc18cb929ef4bd25907ac106927eedfff0896
wilzh40 <wilzh40@gmail.com>
2014-08-21 12:12:40 -0700

    Initial Commit

---
+++ SwiftSkeleton/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  SwiftSkeleton
+//
+//  Created by Wilson Zhao on 8/21/14.
+//  Copyright (c) 2014 Wilson Zhao. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+                            
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.valueForKey("timeStamp").description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
